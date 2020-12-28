ce116c42f000003a09772cfa73aae72473ba3988
TiDu <tidu.nguyen.2000@gmail.com>
2020-12-28 17:03:41 +0700

    fixed the unlucky 13

--- codemonk/the_unlucky_13/main.py
+++ codemonk/the_unlucky_13/main.py
@@ -31,26 +31,37 @@ def o(w='', end='\n'):
 #########################################################
 
 
-M = 10 ** 32
+# M = 10 ** 32
 
 
-def powxy(x, y):
-    if y == 0:
-        return 1
-    if y % 2 == 1:
-        return (x*powxy(x, y-1)) % M
-    t = powxy(x, y/2)
-    return (t*t) % M
+# def powxy(x, y):
+#     if y == 0:
+#         return 1
+#     if y % 2 == 1:
+#         return (x*powxy(x, y-1)) % M
+#     t = powxy(x, y/2)
+#     return (t*t) % M
 
+
+c = 1000000009
+t = '13'
+K = 10
+l_t = len(t)
+R = [0] * (l_t + 1)
+r = len(R)
+
+# O (S * T * K)
 
 for _ in range(ii()):
     n = ii()
-    if not n:
-        o(0)
-    else:
-        n_13 = str(n - 1)
-        l = min(n, 9)
-        n_13_cl = list(map(int, n_13[-l:].zfill(l)))
-        n_13_cl = list(map(lambda x: 9 - x, n_13_cl))
-        n_13_cl[-1] = n_13_cl[-1] + 1
-        o("".join(map(str, n_13_cl)))
+    for j in range(1, l_t + 1):  # 1 -> l_t
+        R[j] = K ** j
+    R[-1] = R[-1] - 1
+
+    for _ in range(l_t + 1, n + 1): # l_t + 1 to n
+        tmp = (R[r - 1] * K) - R[r - l_t]
+        for u in range(l_t):  # 0 -> l_t - 1
+            R[u] = R[u + 1]
+        R[-1] = tmp % c
+
+    o(int(R[-1]))
